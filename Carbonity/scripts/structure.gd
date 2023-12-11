bb5f299e3de72fb4c102f59920b4bc85a5e27a2a
Jerre1 <152609655+Jerre1@users.noreply.github.com>
2023-12-11 14:11:23 +0100

    Add files via upload

---
+++ Carbonity/scripts/structure.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Structure
+
+@export_subgroup("Model")
+@export var model:PackedScene # Model of the structure
+
+@export_subgroup("Gameplay")
+@export var price:int # Price of the structure when building
---
+++ Carbonity/settings.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_home_pressed():
+	get_tree().change_scene_to_file("res://scenes/home.tscn")
+
+
