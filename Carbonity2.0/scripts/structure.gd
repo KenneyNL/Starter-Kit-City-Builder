141ba8475a710df9160594379841b0b2c079f8db
Jerre1 <152609655+Jerre1@users.noreply.github.com>
2023-12-12 13:33:25 +0100

    Delete Carbonity2.0 directory

--- Carbonity2.0/scripts/structure.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name Structure
-
-@export_subgroup("Model")
-@export var model:PackedScene # Model of the structure
-
-@export_subgroup("Gameplay")
-@export var price:int # Price of the structure when building
